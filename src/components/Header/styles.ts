2d4f1927d3d4fed1de1440f4e7535ee6ed235ca8
Tales Garcia <garcia.tales@gmail.com>
2020-11-30 20:23:27 -0300

    feat(styles): add active route style

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -27,6 +27,10 @@ export const Container = styled.div<ContainerProps>`
           margin-left: 32px;
         }
 
+        &.active {
+          border-bottom: 2px solid #FF872C;
+        }
+
         &:hover {
           opacity: 0.6;
         }
